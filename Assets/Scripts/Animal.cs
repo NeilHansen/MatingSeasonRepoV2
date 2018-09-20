6c188eea44415e78c8603e2c2fbdfff04cecfcaf
Erdem Gunay <merdemgunay88@gmail.com>
2018-09-20 13:44:34 -0400

    Perc to Gene implementation. Tested.

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -3,17 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Animal : MonoBehaviour {
+    [SerializeField] bool usePremadeGenes = false;
+
+    [SerializeField] float heatAffinityPerc;
+    [SerializeField] float carnivorityPerc;
+    [SerializeField] float litterSizePerc;
+    [SerializeField] float cancerSusceptibilityPerc;
+
+
+
+
     const int GENE_SIZE = 4;
-    [SerializeField] int[] heatAffinity = new int[GENE_SIZE];
-    [SerializeField] int[] carnivority;
-    [SerializeField] int[] litterSize;
-    [SerializeField] int[] cancerSusceptibility;
+    int[] heatAffinity = new int[GENE_SIZE];
+    int[] carnivority;
+    int[] litterSize;
+    int[] cancerSusceptibility;
 
 
     // Use this for initialization
     void Start () {
-		
+
+        if (usePremadeGenes)
+        {
+            heatAffinity = getGeneFromNum(getNumberofOnes(heatAffinityPerc));
+            carnivority = getGeneFromNum(getNumberofOnes(carnivorityPerc));
+            litterSize = getGeneFromNum(getNumberofOnes(litterSizePerc));
+            cancerSusceptibility = getGeneFromNum(getNumberofOnes(cancerSusceptibilityPerc));
+            // ADD MORE ONCE WE HAVE MORE TRAITS!!!!!!
+
+            Debug.Log("heat gene generated from perc: " + printGene(heatAffinity));
+            Debug.Log("carn gene generated from perc: " + printGene(carnivority));
+            Debug.Log("litt gene generated from perc: " + printGene(litterSize));
+            Debug.Log("canc gene generated from perc: " + printGene(cancerSusceptibility));
+        }
+
 	}
+
+    string printGene(int[] gene)
+    {
+        string temp = "";
+
+        for (int i = 0; i < GENE_SIZE; i++)
+        {
+            temp += gene[i];
+        }
+        return temp;
+    }
+
+    int[] getGeneFromNum(int num)
+    {
+        int[] temp = new int[GENE_SIZE];
+
+        for (int i = 0; i < GENE_SIZE; i++)
+        {
+            if (i < num)
+            {
+                temp[i] = 1;
+            }
+            else
+            {
+                temp[i] = 0;
+            }
+        }
+
+        return temp;
+    }
+
+    int getNumberofOnes(float percWanted)
+    {
+        float difference = 2.0f;
+        int numberOfOnes = 0;
+
+        for (int i = 0; i < GENE_SIZE; i++)
+        {
+            float temp = Mathf.Abs(((float)i / (float)GENE_SIZE) - percWanted);
+
+            if (temp < difference)
+            {
+                difference = temp;
+                numberOfOnes = i;
+            }
+                
+        }
+
+        return numberOfOnes;
+    }
 	
 	// Update is called once per frame
 	void Update () {
