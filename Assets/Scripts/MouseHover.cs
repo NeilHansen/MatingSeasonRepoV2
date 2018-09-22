0b9daf39eab2860dea2ce72c044f63142442f390
Neil Hansen <neilmickneilh12@hotmail.com>
2018-09-22 17:30:20 -0400

    Added icons, and new scene with new layout

---
+++ Assets/Scripts/MouseHover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MouseHover : MonoBehaviour {
+
+    [SerializeField] GameObject AnimalInfoPanel;
+    [SerializeField] Text AnimalNameText;
+    [SerializeField] string PenType;
+
+    void OnMouseOver()
+    {
+        AnimalInfoPanel.SetActive(true);
+        AnimalNameText.text = "" + PenType;
+        Debug.Log("Mouse over " + this.gameObject.name);
+    }
+
+    void OnMouseExit()
+    {
+        AnimalInfoPanel.SetActive(false);
+        Debug.Log("Mouse left " + this.gameObject.name);
+    }
+
+}
