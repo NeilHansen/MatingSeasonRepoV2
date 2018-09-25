cc473c2046c67fd9ac48e5ec89d081c7d254d976
Neil Hansen <neilmickneilh12@hotmail.com>
2018-09-25 16:18:17 -0400

    last text added, gold display

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gold : MonoBehaviour {
+
+
+    [SerializeField] GameObject goldSlider;
+    [SerializeField] Text goldText;
+	
+	// Update is called once per frame
+	void Update () {
+       goldText.text = "GOLD:" + goldSlider.GetComponent<Slider>().value;
+	}
+}
